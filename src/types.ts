602a3594e385767c5e82ff9ee6aea6d538d57128
Eric Afes <eriicafes@gmail.com>
2024-02-08 03:33:29 +0100

    Fix Merge type

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,9 @@
 export type Pretty<T> = { [K in keyof T]: T[K] } & {}
-export type PrettyStringKeys<T> = { [K in keyof T as K extends string ? K : never]: T[K] } & {}
-export type RecordType<T> = IsAny<T> extends true ? Record<string, any> : T
-export type Merge<T, To> = Pretty<RecordType<T> & Omit<RecordType<To>, keyof T>>
-export type IsAny<T> = unknown extends T & string ? true : false;
-export type HasIndexSignature<T> = KnownKey<keyof T> extends never ? true : false
-export type KnownKey<T> = string extends T
+type IsAny<T> = unknown extends T & string ? true : false;
+type RecordType<T> = IsAny<T> extends true ? Record<string, any> : T
+type OmitKnownKey<T, U extends PropertyKey> = { [K in keyof T as Exclude<K, U>]: T[K] }
+export type Merge<T, To> = Pretty<RecordType<T> & OmitKnownKey<RecordType<To>, keyof T>>
+type KnownKey<T> = string extends T
     ? never
     : number extends T
     ? never
@@ -12,3 +11,4 @@ export type KnownKey<T> = string extends T
     ? never
     : T
 export type KnownMappedKeys<T> = { [K in keyof T as KnownKey<K>]: T[K] } & {}
+export type HasIndexSignature<T> = KnownKey<keyof T> extends never ? true : false
